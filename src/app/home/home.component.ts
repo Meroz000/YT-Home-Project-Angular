6f32c38b384e6eacb753b6d07d5f8e9fa5396f29
Meroz000 <samueldebrytosanto12@gmail.com>
2025-06-04 17:05:05 -0300

    Adicionado o componente home

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { VideoComponent } from '../video/video.component';
+
+@Component({
+  selector: 'app-home',
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
